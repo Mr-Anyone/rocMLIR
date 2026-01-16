16d2f4151cad6cabc01bd70b568d734e59173eb1
Vincent <llvm@viceroygroup.ca>
2026-01-16 18:57:39 +0000

    Added missing case

--- mlir/lib/Dialect/Rock/Transforms/GridwiseGemmToBlockwise.cpp
+++ mlir/lib/Dialect/Rock/Transforms/GridwiseGemmToBlockwise.cpp
@@ -2803,7 +2803,7 @@ struct GridwiseAttentionAccelRewritePattern
               invertTransforms(rewriter, loc,
                                gemm0OutSubTileNxMViews.threadSubTile);
           if (failed(gemm0ThreadwiseSubtileViewNxMMaps)) {
-            return failure();
+            return op.emitError("invertTransforms failed");
           }
           Value gemm0ExpNMThreadwiseView =
               transform(rewriter, gemm1RegBufferB,
